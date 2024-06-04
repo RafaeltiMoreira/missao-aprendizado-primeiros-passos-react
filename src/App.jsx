765be54d0f883c3da0366ca3ad1bc6904ed3cc17
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-06-04 01:52:07 -0300

    Criação do componente `<Card />`

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
 
   return (
     <>
-      <div className='card'>
-        <h1>Java</h1>
-        <img src="https://www.salvatore.academy/devmon/1_java.png" width={200} alt="Imagem do Devmon Java" />
-      </div>
+      <Card />
     </>
   )
 }
